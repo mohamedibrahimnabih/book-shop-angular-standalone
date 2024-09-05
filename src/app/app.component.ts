f8e3a7df5d331aad23ddf660661c17547d7e12e5
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-09-05 14:58:04 +0300

    Fix the Paths of Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "../components/navbar/navbar.component";
-import { ProductsComponent } from "../components/products/products.component";
-import { FooterComponent } from "../components/footer/footer.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductsComponent } from './components/products/products.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
